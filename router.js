6bdf318f5afab2050c8cd503a0d05f2ff73b1715
yhwoooo <hwyang0919@naver.com>
2024-08-28 19:57:41 +0900

    google oauth추가

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 import express from "express";
+import passport from "passport";
 import scheduleController from "./routes/apis/schedule.controller.js";
 import scheduleAdminController from "./routes/apis/scheduleAdmin.controller.js";
+import { isNotLoggedIn, isLoggedIn } from "./routes/middlewares.js";
 
 const router = express.Router();
 
@@ -11,4 +13,14 @@ router.post("/schedule", scheduleAdminController.createSchedule);
 router.delete("/schedule/:id", scheduleAdminController.deleteSchedule);
 router.put("/schedule/:id", scheduleAdminController.updateSchedule);
 
+router.get("/user", passport.authenticate("jwt", { session: false }), (req, res) => {
+  res.json(req.user);
+});
+
+router.post("/logout", isLoggedIn, (req, res) => {
+  //   req.logout();
+  //   req.session.destroy();
+  res.status(200).json({ message: "로그아웃 성공" });
+  res.redirect("/");
+});
 export default router;
